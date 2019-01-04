aa8a3e5782fa5a8d7ba7d60793ca270bf6cf3876
rajulonline <rajulonline@gmail.com>
2019-01-03 23:23:47 -0800

    assets pre compiled

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -81,7 +81,7 @@ Rails.application.configure do
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
 
-  config.serve_static_assets = true
+  config.serve_static_files = true
 
   # Use a different logger for distributed setups.
   # require 'syslog/logger'
