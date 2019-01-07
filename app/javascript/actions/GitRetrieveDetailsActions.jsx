0f4e6662c6aa261c5c2ddc1f3ae260b092434fcb
rajulonline <rajulonline@gmail.com>
2019-01-06 21:05:43 -0800

    performance improvements, delete unwanted files

--- app/javascript/actions/GitRetrieveDetailsActions.jsx
+++
@@ -1,8 +0,0 @@
-import API from "../API.jsx";
-
-export default {
-	getAllUserDetails(userName) {
-		console.log(userName,"Here you are at the actions");
-		API.getAllUserDetails(userName);
-	}
-}
--- config/application.rb
+++ config/application.rb
@@ -16,5 +16,6 @@ module FluxReact
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
     config.assets.initialize_on_precompile = false
+    config.middleware.insert_after ActionDispatch::Static, Rack::Deflater
   end
 end
